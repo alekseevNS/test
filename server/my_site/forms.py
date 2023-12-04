2945db54db43d09261a05b3c263d7eef93260534
НС <alekseev@informatics.ru>
2023-12-04 20:13:03 +0300

    Сделали классную форму

--- server/my_site/forms.py
+++ server/my_site/forms.py
@@ -2,5 +2,7 @@ from django import forms
 
 
 class CalcForm(forms.Form):
-    first_number = forms.IntegerField(label="Первое число", min_value=-100, max_value=100)
-    second_number = forms.IntegerField(label="Второе число", min_value=-100, max_value=100)
+    first_number = forms.IntegerField(label="Первое число", min_value=-100, max_value=100,
+                                      widget=forms.NumberInput(attrs={"class": "form-control"}))
+    second_number = forms.IntegerField(label="Второе число", min_value=-100, max_value=100,
+                                       widget=forms.NumberInput(attrs={"class": "form-control"}))
